c58ff088fceec51eb85f8b62aec82dae26b72e94
aeon <jerry73204@gmail.com>
2025-05-10 12:29:29 +0800

    Modify argus config only when the change is not made yet.

--- postinst.sh
+++ postinst.sh
@@ -12,8 +12,11 @@ if [ "$arch" = "arm64" ]; then
     if command -v systemctl >/dev/null && systemctl list-units >/dev/null 2>&1; then
         # Modify argus daemon to avoid timeout when using multiple cameras
         if [ -f "/etc/systemd/system/nvargus-daemon.service" ]; then
-            sed -i '/^\[Service\]$/ a Environment="enableCamInfiniteTimeout=1"' /etc/systemd/system/nvargus-daemon.service
-            systemctl daemon-reload
+            # Check if the line already exists to avoid duplicate entries
+            if ! grep -q "enableCamInfiniteTimeout=1" /etc/systemd/system/nvargus-daemon.service; then
+                sed -i '/^\[Service\]$/ a Environment="enableCamInfiniteTimeout=1"' /etc/systemd/system/nvargus-daemon.service
+                systemctl daemon-reload
+            fi
         fi
 
 	systemctl daemon-reload
@@ -31,6 +34,6 @@ fi
 echo "ZED SDK installation complete. To use the SDK, add your user to the video group with:"
 echo "  sudo usermod -a -G video $(whoami)"
 echo "and then log out and back in."
-echo ''
+echo ""
 echo "To download all AI models and optimize them,"
 echo "  zed_download_ai_models"
